9443e25935583a3880be9fdab94807d48144cf52
carlos tejada <carlosanibaltejada@gmail.com>
2021-12-12 02:36:07 -0300

    funcionamiento de index mapeo de tablas dinamicas

--- js/index.js
+++ js/index.js
@@ -1,33 +1,47 @@
+/*
+ TODO URLS PARA LLAMADAS LOCALES 
+ */
 const timer = new Timer();
 const URL_EVENT = `http://localhost:3000/eventos`;
+const URL_POSICIONES = "http://localhost:3000/posiciones"
 
 $("#chronoExample .startButton").click(function () {
   try {
-    timer.start({ precision: "secondTenths" });
+    timer.start({
+      precision: "secondTenths"
+    });
   } catch (e) {
     console.log("error tonto" + timer.getTimeValues());
   }
 });
 
 $("#chronoExample .pauseButton").click(function () {
-  timer.pause({ precision: "secondTenths" });
+  timer.pause({
+    precision: "secondTenths"
+  });
 });
 
 $("#chronoExample .stopButton").click(function () {
-  timer.stop({ precision: "secondTenths" });
+  timer.stop({
+    precision: "secondTenths"
+  });
 });
 
 $("#chronoExample .resetButton").click(function () {
-  timer.stop({ precision: "secondTenths" });
-  timer.start({ precision: "secondTenths" });
+  timer.stop({
+    precision: "secondTenths"
+  });
+  timer.start({
+    precision: "secondTenths"
+  });
   // timer.pause({ precision: "secondTenths" });
 });
 
 timer.addEventListener("secondTenthsUpdated", function (e) {
   $("#chronoExample .values").html(
     timer
-      .getTimeValues()
-      .toString(["hours", "minutes", "seconds", "secondTenths"])
+    .getTimeValues()
+    .toString(["hours", "minutes", "seconds", "secondTenths"])
   );
 });
 
@@ -51,12 +65,46 @@ const dorsal = $("#numero").keypress(function (evento) {
     }
     postData(URL_EVENT, data)
       .done(function (resp) {
+        cargarEventosExitentes();
         console.log(resp)
         dorsal.val("");
-    });
-    
+      });
+
   }
 });
 
 
+function agregarFila(evento) {
+  const comprobacion: evento.categoriaCorredor;
+  switch (comprobacion == 'Master A-Hombre') {
+    case value:
 
+      break;
+
+    default:
+      break;
+  }
+  const tablaData = $("#tablaCategoriaMasterAMujer");
+  tablaData.fadeIn(3000).append(`<tr>
+  <td>${evento.nombreCorredor}</td>
+  <td>${evento.dorsalCorredor}</td>
+  <td>${evento.time}</td>
+  <td>${evento.categoriaCorredor}</td>
+  <td>${evento.equipoCorredor}</td>
+  </tr>
+  `);
+}
+
+function cargarEventosExitentes() {
+  $.get(URL_POSICIONES, function (response, estado) {
+    if (estado === "success") {
+      const data = response;
+      const tablaData = $("#tablaCategoriaMasterAMujer").empty();
+      for (const dt of data) {
+        agregarFila(dt);
+      }
+    }
+  });
+
+}
+cargarEventosExitentes();
