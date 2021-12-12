8cf00b2210706d60cad64dad5423680a34c08863
carlos tejada <carlosanibaltejada@gmail.com>
2021-12-12 03:12:38 -0300

    agrega tablas de categorias

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 const timer = new Timer();
 const URL_EVENT = `http://localhost:3000/eventos`;
 const URL_POSICIONES = "http://localhost:3000/posiciones"
+const URL_CATEGORIAS= "http://localhost:3000/categorias"
 
 $("#chronoExample .startButton").click(function () {
   try {
@@ -65,8 +66,9 @@ const dorsal = $("#numero").keypress(function (evento) {
     }
     postData(URL_EVENT, data)
       .done(function (resp) {
-        cargarEventosExitentes();
-        console.log(resp)
+        cargarPosicionesMujer();
+        cargarPosicionesHombres();
+        crearTablasCategorias();
         dorsal.val("");
       });
 
@@ -74,18 +76,8 @@ const dorsal = $("#numero").keypress(function (evento) {
 });
 
 
-function agregarFila(evento) {
-  const comprobacion: evento.categoriaCorredor;
-  switch (comprobacion == 'Master A-Hombre') {
-    case value:
-
-      break;
-
-    default:
-      break;
-  }
-  const tablaData = $("#tablaCategoriaMasterAMujer");
-  tablaData.fadeIn(3000).append(`<tr>
+function agregarFila(evento, tabla) {
+  tabla.fadeIn(3000).append(`<tr>
   <td>${evento.nombreCorredor}</td>
   <td>${evento.dorsalCorredor}</td>
   <td>${evento.time}</td>
@@ -95,16 +87,75 @@ function agregarFila(evento) {
   `);
 }
 
-function cargarEventosExitentes() {
-  $.get(URL_POSICIONES, function (response, estado) {
+function cargarPosicionesHombres() {
+  $.get(URL_POSICIONES + '/Hombre', function (response, estado) {
     if (estado === "success") {
       const data = response;
-      const tablaData = $("#tablaCategoriaMasterAMujer").empty();
+      const tablaData = $("#tablaGeneralHombre").empty();
       for (const dt of data) {
-        agregarFila(dt);
+        agregarFila(dt, tablaData);
       }
     }
   });
-
 }
-cargarEventosExitentes();
+
+function cargarPosicionesMujer() {
+  $.get(URL_POSICIONES + '/Mujer', function (response, estado) {
+    if (estado === "success") {
+      const data = response;
+      const tablaData = $("#tablaGeneralMujer").empty();
+      for (const dt of data) {
+        agregarFila(dt, tablaData);
+      }
+    }
+  });
+}
+
+function cargarPorCategoria(categoriaId) {
+  $.get(URL_POSICIONES + '/categoria/' + categoriaId, function (response, estado) {
+    if (estado === "success") {
+      const data = response;
+      const tablaData = $(`#${categoriaId}`).empty();
+      for (const dt of data) {
+        agregarFila(dt, tablaData);
+      }
+    }
+  });
+}
+
+function crearTablasCategorias() {
+  $.get(URL_CATEGORIAS, function (response, estado) {
+    if (estado === "success") {
+      const data = response;
+      const tablaCategoria = $("#tablasCategoria").empty();
+      data.forEach(categoria => {
+        tablaCategoria.append(`
+          <div class="container mt-4">
+            <h2 class="section__titulo section__titulo--variante">Posiciones categoria ${categoria.nombreCategoria} - ${categoria.genero}</h2>
+            <hr class="section__hr section__hr--titulo">
+            <table class="table table-danger table-hover table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Dorsal</th>
+                <th scope="col">Tiempo</th>
+                <th scope="col">Categoria</th>
+                <th scope="col">Equipo</th>
+              </tr>
+            </thead>
+            <tbody id="${categoria.id}">
+            </tbody>
+          </table>
+          </div>
+        `)
+        cargarPorCategoria(categoria.id);
+      })
+    }
+  });
+}
+
+
+
+cargarPosicionesHombres();
+cargarPosicionesMujer();
+crearTablasCategorias();
